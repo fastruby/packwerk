800ae7b33887748d5225e84d52f44699d23fe5cb
Sean Floyd <contact@seanfloyd.dev>
2025-03-04 10:56:30 -0500

    chore: remove dead code

--- install-ruby.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@3)"
-export CFLAGS="-Wno-error=implicit-function-declaration -Wno-error=deprecated-declarations"
-export LDFLAGS="-L$(brew --prefix openssl@3)/lib"
-export CPPFLAGS="-I$(brew --prefix openssl@3)/include"
-mise install ruby@2.3.8
