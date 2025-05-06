e12208ef0c68063bdc42b5fa986324e5443b260e
Sean Floyd <contact@seanfloyd.dev>
2025-05-06 21:41:06 +0200

    Require fewer backports

--- lib/packwerk/backports.rb
+++ lib/packwerk/backports.rb
@@ -1,4 +1,3 @@
-
 # frozen_string_literal: true
 
 # This file centralizes all backports and polyfills needed for 
@@ -16,14 +15,24 @@ module Packwerk
       private
 
       def require_ruby_backports
+        # Ruby 2.4+ backports
+        require "backports/2.4.0/hash/transform_values"
+        require "backports/2.5.0/hash/transform_keys"
+        
         # Ruby 2.5+ backports
         require "backports/2.5.0/kernel/yield_self"
+        require "backports/2.5.0/string/delete_prefix"
+        require "backports/2.5.0/string/delete_suffix"
         
-        # Ruby 3.1+ backports
-        require "backports/3.1"
+        # Ruby 2.6+ backports
+        require "backports/2.6.0/array/to_h"
+        require "backports/2.6.0/hash/merge"
         
-        # Ruby-next for general Ruby compatibility
-        require "ruby-next"
+        # Ruby 3.0+ backports
+        require "backports/3.0.0/hash/except"
+        
+        # Don't require ruby-next as it causes issues with Ruby 2.3.8 in Docker
+        # require "ruby-next"
       end
 
       def require_active_support_backports
